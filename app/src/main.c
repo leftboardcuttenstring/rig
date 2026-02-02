74172c5cf149e0771cacad1e32364f17b9114ea0
veldigfastpack <michael.a.strangewood@gmail.com>
2026-02-02 20:05:04 +0300

    Added the basic project
    Detailed edits and documentation are required

---
+++ app/src/main.c
@@ -0,0 +1,25 @@
+#include "stm32f4xx_hal.h"
+#include "main.h"
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef gpio = {0};
+    gpio.Pin   = GPIO_PIN_5;
+    gpio.Mode  = GPIO_MODE_OUTPUT_PP;
+    gpio.Pull  = GPIO_NOPULL;
+    gpio.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &gpio);
+
+    while (1)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        HAL_Delay(500);
+    }
+}
+
+void _init(void) {}
+
